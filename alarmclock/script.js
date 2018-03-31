b974973348b5f543fa5f7431cb95752fbd468ea3
kternize <35570131+kternize@users.noreply.github.com>
2018-03-30 22:27:40 -0400

    Add files via upload

--- alarmclock/script.js
+++ alarmclock/script.js
@@ -5,8 +5,9 @@ document.getElementById('circle4').style.visibility="hidden";
 document.getElementById('stage4').style.visibility="hidden";
 
 setTimeout(function() {
-	$(".wake-up-text").addClass("alarming");
- 	$(".snooze, .button").addClass("alarming2");
+	// $(".wake-up-text").addClass("alarming");
+ 	$(".button, .button2, .reset").addClass("alarming2");
+ 	$(".button2").addClass("alarming5")
  	$(".circle2").addClass("alarming3")
  	$(".circle1").addClass("alarming4")
  	
@@ -19,12 +20,13 @@ setTimeout(function() {
 
  }, 5000);
  
- $(".button, .snooze").click(function() {
+ $(".button, .button2, .reset").click(function() {
 
- 	$(".wake-up-text").removeClass("alarming");
- 	$(".button, .snooze").removeClass("alarming2");
+ 	// $(".wake-up-text").removeClass("alarming");
+ 	$(".button, .button2, .reset").removeClass("alarming2");
  	$(".circle2").removeClass("alarming3")
  	$(".circle1").removeClass("alarming4")
+ 	$(".button2").removeClass("alarming5")
  
  	document.getElementById('circle3').style.visibility="visible";
  	
